6f1067030f9c29962654d53afab00650b499b7b9
clarissemilk <clarissesantana002@gmail.com>
2025-05-16 19:38:46 -0300

    revisando vetores em java

---
+++ src/Vetor.java
@@ -0,0 +1,9 @@
+public class Vetor {
+    public static void main(String[] args){
+
+        int[] colecaoDeInteiros = {1,2,3,4,5};
+        System.out.println(colecaoDeInteiros[0]);
+
+        System.out.println(colecaoDeInteiros.length);
+    }
+}
