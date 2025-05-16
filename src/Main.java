3db1c1a66edb8ceefd52575874eb4dfdce67c3ca
clarissemilk <clarissesantana002@gmail.com>
2025-05-16 19:29:17 -0300

    revisano condicionais no java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
 
-        double a = 45.5874;
-        System.out.println(a);
+        byte b = 100;
+        short s = 1000;
+        int i = 10000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 20.5;
+        char c = 'A';
+        String str = "Fernanda";
+        boolean bool = true;
+
+        if(b > 150) {
+            System.out.println("verdadeiro");
+        }else if( str == "Fernanda"){
+            System.out.println("fernanda");
+        }
+        else {
+            System.out.println("Falso");
+        }
     }
 }
+
+
